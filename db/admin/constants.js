a97a1f381a918e45c8528b3406539529a25cd2a2
Lewis Ojile <lewisojile@gmail.com>
2021-09-05 12:00:33 +0100

    added list projects

---
+++ db/admin/constants.js
@@ -0,0 +1,9 @@
+const CREATE_PROGRAM = "createProgram";
+const LIST_PROGRAMS = "listPrograms";
+const GET_PROGRAM = "getProgram";
+
+exports.ADMIN_MUTATION_ACTIONS = {
+  CREATE_PROGRAM,
+  LIST_PROGRAMS,
+  GET_PROGRAM,
+};
