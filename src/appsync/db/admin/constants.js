fb68eec86a48026ac656c19db35f027264dbcb0e
lewisojile <mac@macs-MacBook-Pro.local>
2021-11-24 18:21:10 +0100

    workflow test

--- src/appsync/db/admin/constants.js
+++
@@ -1,9 +0,0 @@
-const CREATE_PROGRAM = "createProgram";
-const LIST_PROGRAMS = "listPrograms";
-const GET_PROGRAM = "getProgram";
-
-exports.ADMIN_MUTATION_ACTIONS = {
-  CREATE_PROGRAM,
-  LIST_PROGRAMS,
-  GET_PROGRAM,
-};
