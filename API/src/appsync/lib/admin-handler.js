d43af0b58ac0f143f83b3e4c0834a4f4f3c54cfc
lewisojile <mac@macs-MacBook-Pro.local>
2021-11-24 18:10:10 +0100

    workflow test

---
+++ API/src/appsync/lib/admin-handler.js
@@ -0,0 +1,45 @@
+'use strict';
+const { createProgram } = require('../db/admin/mutations');
+const { listPrograms } = require('../db/admin/queries');
+const { ADMIN_MUTATION_ACTIONS } = require('../db/admin/constants');
+
+exports.graphql = async (event, context, callback) => {
+  console.log(
+    'Got an Invoke Request.Received event {}',
+    JSON.stringify(event, 3),
+  );
+
+  switch (event.field) {
+    case ADMIN_MUTATION_ACTIONS.GET_PROGRAM: {
+      callback(null, { items: null });
+      break;
+    }
+
+    case ADMIN_MUTATION_ACTIONS.LIST_PROGRAMS: {
+      const { body: programs, error } = await listPrograms();
+
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      callback(null, { items: programs });
+      break;
+    }
+
+    case ADMIN_MUTATION_ACTIONS.CREATE_PROGRAM: {
+      const { input: item } = event.arguments;
+      const { body: createdProgram, error } = await createProgram(item);
+
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      callback(null, createdProgram);
+      break;
+    }
+    default: {
+      callback(`Unknown field, unable to resolve ${event.field}`, null);
+      break;
+    }
+  }
+};
